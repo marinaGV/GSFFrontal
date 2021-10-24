b741cf2588704bc5d66ebcb3ecb0293347588de5
AidaKastner <aida.kastner@betterconsultants.es>
2021-10-24 17:02:14 +0200

    Updates

---
+++ precache-manifest.166716bd05e487fb1737a7ebc7bc1e14.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "741ec0156c873044076e3006e043a550",
+    "url": "/GSFFront/index.html"
+  },
+  {
+    "revision": "22783538bffad2b6402f",
+    "url": "/GSFFront/static/css/2.de2856d6.chunk.css"
+  },
+  {
+    "revision": "92ef262ef7d0bbb0ac76",
+    "url": "/GSFFront/static/css/main.b18ee1e6.chunk.css"
+  },
+  {
+    "revision": "22783538bffad2b6402f",
+    "url": "/GSFFront/static/js/2.caaa9093.chunk.js"
+  },
+  {
+    "revision": "4345266b8c018e6077e964aebd994c7b",
+    "url": "/GSFFront/static/js/2.caaa9093.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92ef262ef7d0bbb0ac76",
+    "url": "/GSFFront/static/js/main.3da718e5.chunk.js"
+  },
+  {
+    "revision": "6711213da3f0c6659277",
+    "url": "/GSFFront/static/js/runtime-main.b2d87e03.js"
+  }
+]);
