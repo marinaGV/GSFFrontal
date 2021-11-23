9d9dc1e46db3ea7f25ca4ad955d24f3556577314
Niño Allen Ardiente <ardientenino@gmail.com>
2021-11-23 16:05:02 +0800

    Change language dropdown design

--- src/components/Idioma.js
+++ src/components/Idioma.js
@@ -1,104 +1,101 @@
 import React, { useState } from 'react';
-import '../css/Sidebar.css';
+import '../css/Language.css';
 import styled from 'styled-components';
 import { useTranslation } from 'react-i18next';
-
-
+import { IconContext } from 'react-icons/lib';
+import { NavLink } from 'react-router-dom';
+import * as FaIcons from 'react-icons/fa';
  
 var len ='';
 let idimoSel='1';
 
-
-
 const Idioma = () => {
-
     const [Form, IdiomaSel] = useState({
         idioma: idimoSel
     });
+    const [languagebar, setLanguagebar] = useState(false);
+    let [languageSel, changeLanguageSel] = useState("CAT");
 
+    const showLanguagebar = () => setLanguagebar(!languagebar);
+    const setLanguageSel = (language) => changeLanguageSel(language);
 
     //Maneja la edición e inserción en los forms
-    const handleChange = e =>{
+    const handleChange = e => {
+        IdiomaSel({ [Form.idioma]: e.target.className });
 
-        console.log("Target Value:", e.target.value);
-
-         IdiomaSel({ [Form.idioma]: e.target.value });
-
-         len = e.target.value;
-
-         idimoSel=len;
-
-        console.log("Idioma:", len);
+        len = e.target.className;
+        idimoSel=len;
 
         switch(len) {
-
-            case "1": i18n.changeLanguage("ca");
-            break;
-    
-            case "2": i18n.changeLanguage("es");
-            break;
-    
-            default:  i18n.changeLanguage("ca");
-            break;
-    
+            case "1":
+                i18n.changeLanguage("ca");
+                languageSel = "CAT";
+                break;
+            case "2":
+                i18n.changeLanguage("es");
+                languageSel = "ESP";
+                break;
+            default:
+                i18n.changeLanguage("ca");
+                languageSel = "CAT";
+                break;
         }
 
+        showLanguagebar();
+        setLanguageSel(languageSel);
     }
 
-    const { t, i18n } = useTranslation();
+    const { i18n } = useTranslation();
     
-    console.log("F.Idioma:", Form.idioma);
-    console.log("idimoSel:", idimoSel);
-
     return (
         <IdiomaNav>
-        <div>
-       {/* Comentamos la info del idioma. Se deja comentado por si se quiere incluir
-        <p>Idioma : { i18n.language }</p> */}
-        <span style={{float: 'right'}}>
-
-            <select name="id" id="id" onChange={handleChange} defaultValue={idimoSel} styles={colourStyles} >
-                <option value="1">CAT</option>
-                <option value="2">ESP</option>
-              </select>
-            </span>
-        </div>
+            <IconContext.Provider value={{ color: '#fff' }}>
+                <div className="nav">
+                    <span>{languageSel}</span>
+                    <div className="navicon">
+                        <NavLink to='#'>
+                            <FaIcons.FaBars onClick={showLanguagebar} />
+                        </NavLink>
+                    </div>
+                </div>
+                {(languagebar)
+                    ? <ul>
+                        <DropdownLanguage>
+                            <li onClick={handleChange}>
+                                <div value="1" className="1">CAT</div>
+                            </li>
+                        </DropdownLanguage>
+                        <DropdownLanguage>
+                            <li onClick={handleChange}>
+                                <div value="2" className="2">ESP</div>
+                            </li>
+                        </DropdownLanguage>
+                    </ul>
+                    : null}
+            </IconContext.Provider>
         </IdiomaNav>
     );
-  };
+};
 
-  const IdiomaNav = styled.nav`
-  width: 100px;
-  height: 30vh;
-  display: flex;
-  justify-content: right;
-  position: fixed;
-  top: 0;
-  right: 0;
-  transition: 350ms;
-  z-index: 10;
+const IdiomaNav = styled.nav`
+    width: 100px;
+    position: fixed;
+    top: 0;
+    right: 0;
+    transition: 350ms;
+    z-index: 10;
+    float: right;
 `;
 
-
-const colourStyles = {
-    menuList: styles => ({
-        ...styles,
-        background: 'papayawhip'
-    }),
-    option: (styles, {isFocused, isSelected}) => ({
-        ...styles,
-        background: isFocused
-            ? 'hsla(291, 64%, 42%, 0.5)'
-            : isSelected
-                ? 'hsla(291, 64%, 42%, 1)'
-                : undefined,
-        zIndex: 1
-    }),
-    menu: base => ({
-        ...base,
-        zIndex: 100
-    })
+const DropdownLanguage = styled.nav`
+    background-color: gray;
+    padding: 5px;
+    text-align: center;
+    color: white;
+    &:hover {
+        cursor: pointer;
+        color: #0a58ca;
     }
+`;
 
 export default Idioma
-
